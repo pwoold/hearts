6b5c316d593eab54f9111773b47be1057be6fbdf
Phoebe Wooldridge <wooldridgep@carleton.edu>
2016-03-19 18:12:27 -0500

    created card header and c++ files

---
+++ card.cpp
@@ -0,0 +1,5 @@
+#include "card.h"
+
+card::card() {}
+
+card::card(int val, std::string st) : value(val), suit(st) {}
---
+++ card.h
@@ -0,0 +1,16 @@
+#ifndef CARD_H
+#define CARD_H
+
+#include <string>
+
+class card
+{
+	int value;
+	std::string suit;
+
+public:
+	card();
+	card(int value, std::string suit);
+};
+
+#endif
